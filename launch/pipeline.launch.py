e9ded5fcd537cf666b953a87ca5fa4fbeefe03fa
Afaq <afaqmuk@gmail.com>
2026-02-28 08:59:49 +0000

    Fix launch file package name after rename

--- launch/pipeline.launch.py
+++ launch/pipeline.launch.py
@@ -3,6 +3,6 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 	return LaunchDescription([
-		Node(package='research_robotics', executable='sensor_publisher', name='sensor_publisher'),
-		Node(package='research_robotics', executable='processor_node', name='processor_node', parameters=[{'scale_factor': 3.0}]),
+		Node(package='ros2_modular_data_pipeline', executable='sensor_publisher', name='sensor_publisher'),
+		Node(package='ros2_modular_data_pipeline', executable='processor_node', name='processor_node', parameters=[{'scale_factor': 3.0}]),
 	])
